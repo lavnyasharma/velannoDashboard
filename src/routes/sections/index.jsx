d9d00a5ba9a9354da57ab9548c886ca901bf2b76
Lavanya Sharma <86040864+lavnyasharma@users.noreply.github.com>
2024-10-11 22:20:26 +0530

    updates

---
+++ src/routes/sections/index.jsx
@@ -0,0 +1,51 @@
+import { lazy, Suspense } from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+
+import { MainLayout } from 'src/layouts/main';
+
+import { SplashScreen } from 'src/components/loading-screen';
+import { CONFIG } from 'src/config-global';
+
+import { authRoutes } from './auth';
+import { mainRoutes } from './main';
+import { authDemoRoutes } from './auth-demo';
+import { dashboardRoutes } from './dashboard';
+import { componentsRoutes } from './components';
+
+// ----------------------------------------------------------------------
+
+const HomePage = lazy(() => import('src/pages/home'));
+
+export function Router() {
+  return useRoutes([
+    {
+      path: '/',
+      /**
+       * Skip home page
+       * element: <Navigate to={CONFIG.auth.redirectPath} replace />,
+       */
+      element: (
+        <Suspense fallback={<SplashScreen />}>
+          <MainLayout>
+            <Navigate to={CONFIG.auth.redirectPath} replace />
+          </MainLayout>
+        </Suspense>
+      ),
+    },
+
+    // Auth
+    ...authRoutes,
+
+    // Dashboard
+    ...dashboardRoutes,
+
+    // Main
+    ...mainRoutes,
+
+    // Components
+    ...componentsRoutes,
+
+    // No match
+    { path: '*', element: <Navigate to="/404" replace /> },
+  ]);
+}
